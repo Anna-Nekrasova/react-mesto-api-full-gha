5d6ac728d15005157cf8a09eba57b27dfa0bf378
Anya Nekrasova <annanekrasova22@yandex.ru>
2023-06-30 22:05:01 +0300

    tests2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,28 +4,27 @@ const allowedCors = [
   'localhost:3000',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 // eslint-disable-next-line consistent-return
 const cors = (req, res, next) => {
   const { origin } = req.headers;
 
-  res.header('Access-Control-Allow-Credentials', true);
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    return res.status(200).send();
   }
 
   next();
 };
 
-module.exports = cors;
+module.exports = { cors };
